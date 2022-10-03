1a7d11d5f884202b5a5f5c983563c8ccb3c830c7
MargauxRenoir <margaux.renoir1@gmail.com>
2022-10-03 08:17:08 -0400

    J1C5

---
+++ j1/exo5/main.c
@@ -0,0 +1,16 @@
+//int o_charpos(char *str, char c) qui accepte une chaîne de caractères str et un char c, et qui renvoie la position de c dans str si c est inclus dans str, et -1 si c n'est pas dans str
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int o_charpos(char *str, char c){
+
+int res = -1; 
+for(int i = 0; i < strlen(str); i++){
+	if (str[i] == c){
+		res = i;
+	}
+}
+return res;
+}
+
