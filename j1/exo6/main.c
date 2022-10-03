a8a580f482f088484353ece9128cc6509d1a72b6
MargauxRenoir <margaux.renoir1@gmail.com>
2022-10-03 08:46:07 -0400

    J1C6

---
+++ j1/exo6/main.c
@@ -0,0 +1,17 @@
+//Faire un programme qui prend l'input d'un utilisateur et print cet input en retour. Vous avez le droit à scanf et write
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( int argc, char * argv[] ) {
+
+char *question = "Quel est votre nom? ";
+char *nom;
+
+write(1,question,strlen(question));
+scanf("%s", nom);
+
+write(1,nom,strlen(nom));
+return 0;
+}
