4bf817165ee6c31b953b12d6ff860f75c6d20dd7
MargauxRenoir <margaux.renoir1@gmail.com>
2022-10-03 06:04:03 -0400

    J1C3

--- j1/exo3/main.c
+++ j1/exo3/main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 int main() {
 int i = 57;
-int cpt = ;
+int cpt = 9;
 
-while (cpt < 9)
+write(1,"10\n",3);
+
+while (cpt != 0)
 {
    
 write(1,&i,1);
 write(1,"\n",1);
-i++;
-cpt++;
+i--;
+cpt--;
 }
 
-write(1,"10\n",2);
 return 0;
 }
 
