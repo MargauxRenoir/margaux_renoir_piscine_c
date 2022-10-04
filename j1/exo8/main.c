b5267c97bc179aaa036df32b96ed7008fe1aaa21
MargauxRenoir <margaux.renoir1@gmail.com>
2022-10-04 03:17:20 -0400

    J1C8 corr

--- j1/exo8/main.c
+++ j1/exo8/main.c
@@ -1,20 +1,19 @@
 //Créez une fonction "bool o_pow3(int n)", qui étant donné un entier positif, indique si c'est une puissance de 3. Vous devez utiliser la récursion.
-#include <stdio.h>
-#include <stdlib.h>
+
 #include <stdbool.h> 
 
 bool o_pow3(int n)
 {
-    if(n%3 ==1)
+    if(n == 1)
     {
         return true;
     }
 
-    if(n%3 != 0)
+    if(n%3 == 0)
     {
-        return false;
+        return o_pow3(n/3);
     }
 
-    return o_pow3(n/3);
+    return false;
 }
 
