56b58807fcf00e48ebfb58921745a40e9a8bba30
MargauxRenoir <margaux.renoir1@gmail.com>
2022-10-04 02:59:42 -0400

    J1C7 corr

--- j1/exo7/main.c
+++ j1/exo7/main.c
@@ -1,17 +1,17 @@
 //Créez une fonction "long o_arrsum(int *arr)" qui étant donné un array d'int, renvoie la somme de ces int
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-long o_arrsum(int *arr, int taille){
+long o_arrsum(int *arr, int taille)
+{
+       int som = 0;
 
-int som = 0;
+       for(int i =0; i < taille; i++)
+       {
+              som = som + arr[i];
+       }       
 
-for(int i =0; i < taille; i++){
-       som = som + arr[i];
-}       
-
-return som;
+       return som;
 
 }
 
