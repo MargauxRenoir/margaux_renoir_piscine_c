7455b704148c75805f4bd9b1e7ba1fd25277ed44
MargauxRenoir <98284566+MargauxRenoir@users.noreply.github.com>
2022-10-09 20:39:12 +0100

    J4_Projet_Final

--- j4/projet2/main.c
+++ j4/projet2/main.c
@@ -40,75 +40,154 @@ int main(int argc, char *argv[])
     int *scores = malloc(nbJ * sizeof(char *));
     int score = 0;
 
+    printf("_______________________________________________________________________________________________________________________________________\n");
+    printf("|                                                                                                                                      |\n");
+    printf("|                                                BIENVENUE AU SUPERQUIZZ !                                                             |\n");
+    printf("|______________________________________________________________________________________________________________________________________|\n");
+
     printf("Quel est le nombre de joueurs ? \n");
     scanf("%d",&nbJ);
     //Creation tab de pseudo
     char **pseudos = nb_joueur(nbJ);
 
-    printf("Ok ! Combien voulez vous de questions ? \n");
-    scanf("%d",&nb_quest);
-    if (!nb_joueur || !nb_quest) {
-        printf("Erreur dans le nombre de joueur ou de question\n");
-        exit(0);
-    }
-    
-    srand (time (NULL));
-    num_quest =rand()%20;
+    int continuer = 1;
 
-    printf("Ok ! Le jeu commence !\n");
-
-    for(int i = 0; i < nb_quest; i++)
-    {
-        //printf("je suis dans la boucle");
-        //printf("num_quest = %d \n",num_quest);
-
-        printf("La question %i est : %s \n",i+1,tab_questions[num_quest].lib);
-
-        printf("Vous avez le choix entre ces reponses : \n");
-        for(int i = 0; i < 3; i++)
+        for(int i = 0; i < nbJ; i++)
         {
-            printf("%d. %s \n",i+1,tab_questions[num_quest].choix[i]);
+            scores[i] = 0;
+        }
+
+    while(continuer)
+    {
+        // On réinialise 
+        for(int i = 0; i < nbJ; i++)
+        {
+            scores[i] = 0;
+        }
+        int score = 0;
+
+        printf("Ok ! Combien voulez vous de questions ? \n");
+        scanf("%d",&nb_quest);
+        if (!nb_joueur || !nb_quest) {
+            printf("Erreur dans le nombre de joueur ou de question\n");
+            exit(0);
+        }
+        
+        srand (time (NULL));
+        num_quest =rand()%28;
+
+        printf("Ok ! Le jeu commence !\n");
+
+        for(int i = 0; i < nb_quest; i++)
+        {
+            printf("_______________________________________________________________________________________________________________________________________\n");
+            printf("|                                                                                                                                      |\n");
+            printf("|                                                      MANCHE %d                                                                        |\n",i+1);
+            printf("|______________________________________________________________________________________________________________________________________|\n");
+
+            printf("La question %i est : %s \n",i+1,tab_questions[num_quest].lib);
+
+            printf("Vous avez le choix entre ces reponses : \n");
+            for(int i = 0; i < 3; i++)
+            {
+                printf("%d. %s \n",i+1,tab_questions[num_quest].choix[i]);
+            }
+
+            for(int i = 0; i < nbJ; i++)
+            {
+                printf("Votre réponse %s ?\n",pseudos[i]);
+                scanf("%d",&rep);
+                reps[i] = rep;
+            }
+
+            for(int i = 0; i < nbJ; i++)
+            {
+                if(reps[i] == tab_questions[num_quest].rep)
+                {
+                    scores[i]++;
+                    printf("Bravo %s ! Votre score est maintenant de : %d \n",pseudos[i],scores[i]);
+                }
+
+                if(reps[i] != tab_questions[num_quest].rep)
+                {
+                    printf("Désolée %s.. Votre score reste à : %d \n",pseudos[i],scores[i]);
+                }
+            }
+        
+            num_quest =rand()%20;
+        }
+
+        //On trouve le ou les gagnants
+        int nb_gagnant = 0;
+        int * gagnant = malloc(nbJ * sizeof(char *));
+        for(int i = 0; i < nbJ; i++)
+        {
+            if(scores[i] > score)
+            {
+                score = scores[i];
+            }    
+            gagnant[i] = i;
         }
 
         for(int i = 0; i < nbJ; i++)
         {
-            printf("Votre réponse %s ?\n",pseudos[i]);
-            scanf("%d",&rep);
-            reps[i] = rep;
-        }
-
-        for(int i = 0; i < nbJ; i++)
-        {
-            if(reps[i] == tab_questions[num_quest].rep)
+            if(score == scores[i])
             {
-                scores[i]++;
-                printf("Bravo %s ! Votre score est maintenant de : %d ?\n",pseudos[i],scores[i]);
-            }
-
-            if(reps[i] != tab_questions[num_quest].rep)
-            {
-                printf("Désolée %s.. Votre score reste à : %d ?\n",pseudos[i],scores[i]);
-            }
+                score = scores[i];
+                nb_gagnant = nb_gagnant +1;
+            }    
         }
-    
-        num_quest =rand()%20;
-     }
-
-    //On trouve le gagnant
-    int gagnant;
-    for(int i = 0; i < nbJ; i++)
-    {
-        if(scores[i] > score)
+        
+        if(score == 0)
         {
-            score = scores[i];
-            gagnant = i;
+            printf("_______________________________________________________________________________________________________________________________________\n");
+            printf("|                                                                                                                                      |\n");
+            printf("|                                            Zut.. Personne n'a gagné cette partie...                                                   |\n");
+            printf("|______________________________________________________________________________________________________________________________________|\n");
+
+            printf("Voulez-vous une belle ? Oui : 1 Non : 0 \n");
+            scanf("%d",&continuer);
         }
+
+        else
+        {
+            if(nb_gagnant == 1)
+            {
+                printf("_______________________________________________________________________________________________________________________________________\n");
+                printf("|                                                                                                                                      |\n");
+                printf("|                                            BRAVO %s, vous avez gagné cette partie !                                                   |\n",pseudos[gagnant[1]]);
+                printf("|______________________________________________________________________________________________________________________________________|\n");
+                printf("Voulez-vous une revanche ? Oui : 1 Non : 0 \n");
+                scanf("%d",&continuer);
+            }
+            else
+            {
+                printf("_______________________________________________________________________________________________________________________________________\n");
+                printf("|                                                                                                                                      |\n");
+                printf("|                                            Bravo ");
+                int i;
+                for(i = 0; i < nb_gagnant - 1; i++)
+                {
+                    if(scores[i] == score)
+                    {
+                        gagnant[i] = i;
+                    }
+                    printf("%s,",pseudos[gagnant[i]]);
+                }
+                printf("%s, vous avez gagné !                                                              |\n",pseudos[gagnant[i]]);
+                printf("|______________________________________________________________________________________________________________________________________|\n");
+                printf("Voulez-vous une revanche ? Oui : 1 Non : 0 \n");
+                scanf("%d",&continuer);
+            }
+        }
+
     }
 
-    
-    printf("BRAVO %s, vous avez gagné !",pseudos[gagnant]);
+    printf("Merci d'avoir joué avec SUPERQUIZZ, on espère que ce jeu vous aura plus ! :) \n");
 
-
+    for (int i = 0; i < nbJ; i++)
+        free(pseudos[i]);
+    free(pseudos);
+    free(scores);
     return 0;
 }
-
